d08d878967063f812f1cce194ba884b0d8103fbf
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2022-09-13 02:22:00 +0530

    logic.js fixed, UI change

--- logic.js
+++ logic.js
@@ -47,13 +47,13 @@ function calculation(i,q,c)
         var loss = (i - c)* q;
         var lossPercentage =(loss/i)*100;
         output.style.color = 'red';
-        output.innerHTML = `You are in Loss! The loss is ${loss} and loss percentage ${lossPercentage} 🎉`;
+        output.innerHTML = `You are in Loss! The loss is ${loss} and loss percentage ${lossPercentage} 😿`;
     
     }else if( c>i) {
           var profit = (c - i)*q;
           var profitPercentage = (profit/i)*100;
           output.style.color = 'green';
-        output.innerText = `You are in Profit! The Profit is ${profit} and profit percentage is ${profitPercentage}. 😿`;
+        output.innerText = `You are in Profit! The Profit is ${profit} and profit percentage is ${profitPercentage}. 🎉`;
     }
     else{
         output.innerHTML ="it's all about risk 🚀";
