8e1765ebf729c986db2e8cb05e6ab3cbb3a92786
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2022-09-13 02:17:58 +0530

    formula added

---
+++ logic.js
@@ -0,0 +1,61 @@
+var intialPrice = document.querySelector("#intial-price");
+var stocksQuantity = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var submit = document.querySelector("#submit");
+var output = document.querySelector("#output");
+var page = document.querySelector("#fullpage");
+
+//botton function 
+submit.addEventListener('click', checkInput);
+
+function checkInput() {
+    if ((intialPrice.value == '') || (stocksQuantity.value == '') || (currentPrice.value == '')) {
+        alert('Inputs can not be left blank');
+    }
+    else {
+        submitHandler();
+    }
+}
+
+
+function submitHandler() {
+    var ip = Number(intialPrice.value);
+    var qty = Number(stocksQuantity.value);
+    var curr = Number(currentPrice.value);
+    check(ip, qty, curr)
+}
+
+function check(ip, qty, curr){
+    if(ip > 0 && qty > 0 && curr >0){
+        calculation(ip, qty, curr)
+    }
+    else{
+        output.style.color = "red";
+        output.innerHTML ="Please input values correct"
+    }
+}
+
+
+
+
+
+
+//logic building
+function calculation(i,q,c)
+ {
+    if (i > c) {
+        var loss = (i - c)* q;
+        var lossPercentage =(loss/i)*100;
+        output.style.color = 'red';
+        output.innerHTML = `You are in Loss! The loss is ${loss} and loss percentage ${lossPercentage} 🎉`;
+    
+    }else if( c>i) {
+          var profit = (c - i)*q;
+          var profitPercentage = (profit/i)*100;
+          output.style.color = 'green';
+        output.innerText = `You are in Profit! The Profit is ${profit} and profit percentage is ${profitPercentage}. 😿`;
+    }
+    else{
+        output.innerHTML ="it's all about risk 🚀";
+    }
+ }
